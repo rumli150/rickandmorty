b9685f4d34d4b28adc6ab09bd5bfcdc690b72490
Kollár Bence <kollar.bence@innobyte.hu>
2023-07-18 08:10:07 +0200

    Location profilon, most már képpekkel mutatja a lakókat és rájuk kattintva, bedobja az oldalukat, ha élnek

--- src/app/location-list/location-profile/resident/resident.component.ts
+++ src/app/location-list/location-profile/resident/resident.component.ts
@@ -1,5 +1,8 @@
 import { Component, Input } from '@angular/core';
+import { Router } from '@angular/router';
+import { Subscription } from 'rxjs';
 import { Character } from 'src/app/character-list/character.model';
+import { DataStorageService } from 'src/app/shared/data-storage.service';
 
 @Component({
   selector: 'app-resident',
@@ -8,18 +11,36 @@ import { Character } from 'src/app/character-list/character.model';
 })
 export class ResidentComponent {
   @Input() character: Character;
-  @Input() indexStr : string;
-  charIndex : number
+  @Input() indexStr: string;
+  charIndex: number;
+  image = 'https://cdn.icon-icons.com/icons2/1898/PNG/512/planet_121108.png'
+  sub : Subscription
 
-  constructor(){
-    
-  }
+  constructor(
+    private dataStorageService: DataStorageService,
+    private router : Router
+  ) {}
 
   ngOnInit() {
-    let indexList = this.indexStr.split('/')
-    this.charIndex = + indexList[indexList.length-1]
-    console.log(this.charIndex)
-
+    let indexList = this.indexStr.split('/');
+    this.charIndex = +indexList[indexList.length - 1];
+    this.sub = this.dataStorageService.getCharacter(this.charIndex).subscribe(character =>{
+      this.image = character.image
+    })
+    
+  }
+  onOpenResident() {
+    this.sub.unsubscribe()
+    this.sub = this.dataStorageService.getCharacter(this.charIndex).subscribe(character =>{
+      this.image = character.image
+      if(character.status == 'Alive'){
+        this.router.navigate(['/characters',this.charIndex])
+      }else{
+        alert('Character is dead, or unkown')
+      }
+    })
+  }
+  ngOnDestroy(){
 
   }
 }
