b34b4a22320de18941fd9df7f107d1584c578f0f
Kollár Bence <kollar.bence@innobyte.hu>
2023-07-19 11:03:33 +0200

    kicsi fordítás: Karakter oldal szűrője

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,7 +8,7 @@ import { TranslateService } from '@ngx-translate/core';
 })
 export class AppComponent {
   constructor(private translate: TranslateService){
-    this.translate.setDefaultLang('hu')
+    this.translate.setDefaultLang('en')
   }
   switchLanguage(language: string){
     this.translate.use(language)
